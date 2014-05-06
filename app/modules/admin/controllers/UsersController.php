c2f75f55ae4d36ea137224d32bee90dad0469892
hero2510 <thanhtungvl2510@gmail.com>
2014-05-06 18:06:32 +0700

    update

--- app/modules/admin/controllers/UsersController.php
+++ app/modules/admin/controllers/UsersController.php
@@ -8,6 +8,8 @@ class UsersController extends \BaseController{
 
 	public function showUsers(){
 		// $users = \DB::select('SELECT * FROM person');
+		// $route = new \Route;
+		// var_dump(get_class());
 		$users = UsersModel::all();
 
 		return View::make('admin::user.list', array('users' => $users));
