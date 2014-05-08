2ae13538f8e9f1aa63906d1d18684d493246e9d4
hero2510 <thanhtungvl2510@gmail.com>
2014-05-08 18:00:39 +0700

    update

--- app/modules/admin/views/partials/breadcrumbs.blade.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Breadcrumbs::register('home', function($breadcrumbs){
-	$breadcrumbs->push('Home', route('home'));
-});
-
-Breadcrumbs::register('users', function($breadcrumbs){
-	$breadcrumbs->push('User', route('users'));
-});
